3e38f595c14d4d15d45c0653ed8cc7d79e80b8b4
Lauri Paronen <lauri.m.paronen@tuni.fi>
2023-10-31 04:30:18 +0200

    Add & link vertex & fragment shader files

--- src/app/Components/AudioVisualizerPage/ShaderCanvas/fractal-visualizer.js
+++ src/app/Components/AudioVisualizerPage/ShaderCanvas/fractal-visualizer.js
@@ -3,21 +3,87 @@
  * ignore this file for now
  */
 
-import React, { useRef, useEffect, use } from "react";
+import React, { useRef, useEffect, useMemo } from "react";
 import { extend } from "@react-three/fiber";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Box, OrbitControls } from "@react-three/drei";
 extend({ Canvas, Box, OrbitControls });
 
+import fragmentShader from "./fragment-shader";
+import vertexShader from "./vertex-shader";
+
 import * as THREE from "three";
+
+//tring to draw a basic fractal first
+var camera, scene, renderer;
+var geometry, material, mesh;
+var uniforms;
+
+const aspect = window.innerWidth / window.innerHeight;
+
+
+const Plane = () => {
+    const ref = useRef(null);
+    const uniforms = useMemo(
+        () => ({
+            u_time: { value: 1.0 },
+            u_resolution: { value: new THREE.Vector2() },
+        }),
+        []
+    );
+
+    useFrame((state) => {
+        ref.current.material.uniforms.u_time.value = state.clock.getElapsedTime();
+    });
+
+    return (
+        <mesh ref={ref} position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]} scale={1.5} >
+            <planeGeometry args={[1, 1, 32, 32]} />
+            <shaderMaterial
+                fragmentShader={fragmentShader}
+                vertexShader={vertexShader}
+                uniforms={uniforms}
+                wireframe
+            />
+        </mesh>
+    );
+};
+
 
 const FractalVisualizer = (props) => {
     const meshRef = useRef(null);
     const audioRef = props.audioRef;
     const src = props.src;
     const file = props.file;
+    
 
     console.log("FractalVisualizer props: ", props);
+
+    // testing with a basic shape first
+    return (
+        <div className="fractal-visualizer">
+            <Canvas camera={{ position: [1.0, 1.5, 1.0]}}>
+                <ambientLight />
+                <pointLight position={[10, 10, 10]} />
+                <Plane />
+                <OrbitControls />
+            </Canvas>
+        </div>
+
+
+/*
+    const uniforms = useMemo(
+        () => {
+            const uniforms = {
+                u_time: { value: 1.0 },
+                u_resolution: { value: new THREE.Vector2() },
+            }
+            console.log('uniforms', uniforms)
+            return uniforms
+        },
+        []
+    );
+    
 
     return (
         <div className="fractal-visualizer">
@@ -28,8 +94,16 @@ const FractalVisualizer = (props) => {
                     <meshStandardMaterial color={0x00ff00} />
                 </Box>
                 <OrbitControls />
+                <mesh ref={meshRef}>
+                    <shaderMaterial
+                        fragmentShader={fragmentShader}
+                        vertexShader={vertexShader}
+                        uniforms={uniforms}
+                    />
+                </mesh>
             </Canvas>
         </div>
+        */
     );
 }
 
---
+++ src/app/Components/AudioVisualizerPage/ShaderCanvas/vertex-shader.js
@@ -0,0 +1,22 @@
+const vertexShader = `
+varying vec2 vUv;
+uniform float u_time;
+uniform float u_resolution;
+
+
+void main() {
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    modelPosition.y += sin(modelPosition.x * 4.0 + u_time * 2.0) * 0.2;
+    
+    // Uncomment the code and hit the refresh button below for a more complex effect 🪄
+    modelPosition.y += sin(modelPosition.z * 6.0 + u_time * 2.0) * 0.1;
+  
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+  
+    gl_Position = projectedPosition;
+  }
+
+`;
+
+export default vertexShader;
